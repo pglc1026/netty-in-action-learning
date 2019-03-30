90a8a0fe7ead962b89a82509d15743944704968d
pglc1026 <pglc1026@163.com>
2019-03-30 11:16:24 +0800

    modify some handler as inner class

--- chapter11/src/main/java/nia/chapter11/WebSocketServerInitializer.java
+++ chapter11/src/main/java/nia/chapter11/WebSocketServerInitializer.java
@@ -1,10 +1,11 @@
 package nia.chapter11;
 
-import io.netty.channel.Channel;
-import io.netty.channel.ChannelInitializer;
-import io.netty.channel.ChannelPipeline;
+import io.netty.channel.*;
 import io.netty.handler.codec.http.HttpObjectAggregator;
 import io.netty.handler.codec.http.HttpServerCodec;
+import io.netty.handler.codec.http.websocketx.BinaryWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.ContinuationWebSocketFrame;
+import io.netty.handler.codec.http.websocketx.TextWebSocketFrame;
 import io.netty.handler.codec.http.websocketx.WebSocketServerProtocolHandler;
 
 /**
@@ -25,5 +26,29 @@ public class WebSocketServerInitializer extends ChannelInitializer<Channel> {
                 new TextFrameHandler(),
                 new BinaryFrameHandler(),
                 new ContinuationFrameHandler());
+    }
+
+    public final class TextFrameHandler extends SimpleChannelInboundHandler<TextWebSocketFrame> {
+
+        @Override
+        protected void channelRead0(ChannelHandlerContext channelHandlerContext, TextWebSocketFrame textWebSocketFrame) throws Exception {
+            // handle text frame
+        }
+    }
+
+    public final class BinaryFrameHandler extends SimpleChannelInboundHandler<BinaryWebSocketFrame> {
+
+        @Override
+        protected void channelRead0(ChannelHandlerContext channelHandlerContext, BinaryWebSocketFrame binaryWebSocketFrame) throws Exception {
+            // handle binary frame
+        }
+    }
+
+    public final class ContinuationFrameHandler extends SimpleChannelInboundHandler<ContinuationWebSocketFrame> {
+
+        @Override
+        protected void channelRead0(ChannelHandlerContext channelHandlerContext, ContinuationWebSocketFrame continuationWebSocketFrame) throws Exception {
+            // handle continuation frame
+        }
     }
 }
